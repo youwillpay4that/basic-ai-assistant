a83a7a75b816996be94c61d679c4749694666fd4
youwillpay4that <naveentuyo@gmail.com>
2024-07-20 18:34:23 -0400

    Updated configs

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 model_name = "gemini-1.5-flash"
-max_output_tokens = 200 # 100 - 400 is my reccomended range.
-temperature = 1.5 #  0-2. 2 is very creative responses, 0 is determinsitic
+max_output_tokens = 200 
+temperature = 1.5
 wake_word = "hey jarvis" 
-personality = "Be creative and a little sarcastic. Keep answers SHORT. "#"Keep the response SHORT and to the POINT. "
-print_output = True # text prompts
+personality = "Be creative and a little sarcastic. Keep answers SHORT. "
+print_output = True
